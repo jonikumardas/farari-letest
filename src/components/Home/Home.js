a067bfe8dc3c2c27ee095852d38f195fdb4dd6b1
jonikumardas <jonikumardas213@gmail.com>
2022-04-05 12:22:40 +0600

    home section end

---
+++ src/components/Home/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import img from '../../Assate/ass.jpg'
+import './Home.css';
+const Home = () => {
+    return (
+        <div>     
+        <div className='container'>
+            <div className="aritical-container">
+                <h1>Letest <span>Farary</span> car </h1>
+                <p>
+                    This is our letest Farary car A full-face helmet does little to take the edge off the most rpm shriek of a naturally aspirated huge liter flat-six when it's mounted amidships just behind your seat, as it is in the 2022 Porsche 718 Cayman GT4 RS. We start to wish we'd also worn earplugs really, because its glorious sounds may well be worth hearing aids in  as we feed in the power exiting a slow corner, then bang through the gears on our way to storming headlong into a banked sweeper at Streets of Willow Springs raceway.
+                </p>
+                <button><p>Buy now</p></button>
+          </div>
+            <div className="image-container">
+
+                <img src={img} alt="" />
+
+                
+          </div>
+            </div>
+            </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Manu/Manu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Manu = () => {
+    return (
+        <div>
+           <a href="./home">Home</a>
+           <a href="./revew">Revew</a>
+           <a href="./blog">Blog</a>
+           <a href="./ about">About</a>
+        </div>
+    );
+};
+
+export default Manu;
